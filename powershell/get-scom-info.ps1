5dfda8941514476fa4a134e2ca36e62d748a6af8
Krunal-smiles <34149628+Krunal-smiles@users.noreply.github.com>
2021-02-03 14:05:10 +1100

    mma version and mgmt group

---
+++ powershell/get-scom-info.ps1
@@ -0,0 +1,60 @@
+[CmdletBinding()]
+param([Parameter(Mandatory=$True,Position=1)][string]$csvfile)
+
+$computers = Import-Csv -Path $csvfile
+$array = @()
+foreach($pc in $computers){
+    $computername=$pc.computername
+    
+    #Define the variable to hold the location of Currently Installed Programs
+    $UninstallKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+
+    #Create an instance of the Registry Object and open the HKLM base key
+    $reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername)
+
+    #Drill down into the Uninstall key using the OpenSubKey Method
+    $regkey=$reg.OpenSubKey($UninstallKey) 
+
+    #Retrieve an array of string that contain all the subkey names
+    $subkeys=$regkey.GetSubKeyNames()
+
+    #Open each Subkey and use GetValue Method to return the required values for each
+    foreach($key in $subkeys){
+        $thisKey=$UninstallKey+"\\"+$key
+        $thisSubKey=$reg.OpenSubKey($thisKey)
+        $dName=$thisSubKey.GetValue("DisplayName")
+        if($dName -eq "Microsoft Monitoring Agent") {
+            try {
+                $mgs=(Invoke-Command -ComputerName $computername -ScriptBlock { (New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg').GetManagementGroups() })
+            } catch {}
+            
+            if ($mgs) {
+                Foreach($mg in $mgs) {
+                    $obj = New-Object PSObject
+                    $obj = $mg 
+                    $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $computername
+                    $obj | Add-Member -MemberType NoteProperty -Name "Name" -Value $($thisSubKey.GetValue("DisplayName"))
+                    $obj | Add-Member -MemberType NoteProperty -Name "Version" -Value $($thisSubKey.GetValue("DisplayVersion"))
+                    #$obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($thisSubKey.GetValue("InstallLocation"))
+                    #$obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($thisSubKey.GetValue("Publisher"))
+                    $array += $obj
+                }
+            } else {
+                $obj = New-Object PSObject            
+                $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $computername
+                $obj | Add-Member -MemberType NoteProperty -Name "Name" -Value $($thisSubKey.GetValue("DisplayName"))
+                $obj | Add-Member -MemberType NoteProperty -Name "Version" -Value $($thisSubKey.GetValue("DisplayVersion"))
+                $obj | Add-Member -MemberType NoteProperty -Name "managementGroupName" -Value ""
+                $obj | Add-Member -MemberType NoteProperty -Name "ManagementServer" -Value ""
+                $obj | Add-Member -MemberType NoteProperty -Name "managementServerPort" -Value ""
+                $obj | Add-Member -MemberType NoteProperty -Name "IsManagementGroupFromActiveDirectory" -Value ""
+                $obj | Add-Member -MemberType NoteProperty -Name "ActionAccount" -Value ""
+                #$obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($thisSubKey.GetValue("InstallLocation"))
+                #$obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($thisSubKey.GetValue("Publisher"))
+                $array += $obj
+            }
+        }
+    }
+}
+
+$array | Format-Table -auto
